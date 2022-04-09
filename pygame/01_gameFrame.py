219337c4f07b1f6b8886c73d486c444f9f6d1449
yulepark <the.0920.yule@gmail.com>
2022-04-09 12:29:38 +0900

    0409

--- pygame/01_gameFrame.py
+++ pygame/01_gameFrame.py
@@ -6,13 +6,12 @@ screen_width = 480
 screen_height = 640
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-pygame.display.set_caption("똥피하기-코드플레이")
+pygame.display.set_caption("명사수의 총알피하기-코플")
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
 
 pygame.quit()
