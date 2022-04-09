5d53cd67403dfc3231872a90f8d519b08fe77c70
yulepark <the.0920.yule@gmail.com>
2022-04-09 11:35:32 +0900

    escape

---
+++ 01_gameFrame.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import pygame
+pygame.init()
+
+screen_width = 480
+screen_height = 640
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption("똥피하기-코드플레이")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+pygame.quit()
