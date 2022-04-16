8fee3c8f11bcbc2fe5fbafd8594770de0b43e555
yulepark <the.0920.yule@gmail.com>
2022-04-16 12:28:17 +0900

    0416

---
+++ pygame/03_mainsprite.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import pygame
+pygame.init()
+
+screen_width = 480
+screen_height = 640
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption("명사수의 총알피하기-코플")
+
+bg = pygame.image.load("pygame/source/bg.png")
+
+character = pygame.image.load("pygame/source/character.png")
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_xPos = (screen_width / 2) - (character_width / 2)
+character_yPos = screen_height  character_height
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.blit(bg, (0, 0))
+    screen.blit(character, (character_xPos, character_yPos))
+
+    pygame.display.update()
+
+
+pygame.quit()
