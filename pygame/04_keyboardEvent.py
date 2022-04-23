03528328fb00f783a7785c4919d2bf41d5f72cfe
yulepark <the.0920.yule@gmail.com>
2022-04-23 12:29:22 +0900

    4월23일 키보드로 스프라이트 조종

---
+++ pygame/04_keyboardEvent.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from calendar import c
+import pygame
+pygame.init()
+
+screen_width = 480
+screen_height = 640
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption("명사수의 총알피하기-코플")
+
+bg = pygame.image.load("pygame/source/bg.png")
+
+character = pygame.image.load("pygame/source/character.png")
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_xPos = (screen_width / 2) - (character_width / 2)
+character_yPos = screen_height - character_height
+
+to_x = 0 
+to_y = 0
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                to_x -= 1
+            elif event.key == pygame.K_RIGHT:
+                to_x += 1
+            elif event.key == pygame.K_UP:
+                to_y -= 1
+            elif event.key == pygame.K_DOWN:
+                to_y += 1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                to_y = 0
+
+    character_xPos += to_x
+    character_yPos += to_y
+
+    if character_xPos < 0:
+        character_xPos = 0
+    elif character_xPos > screen_width - character_width:
+        character_xPos = screen_width - character_width
+    if character_yPos < 0:
+        character_xPos = 0
+    elif character_yPos > screen_height - character_height:
+        character_yPos = screen_height - character_height
+
+    screen.blit(bg, (0, 0))
+    screen.blit(character, (character_xPos, character_yPos))
+
+    pygame.display.update()
+
+
+pygame.quit()
